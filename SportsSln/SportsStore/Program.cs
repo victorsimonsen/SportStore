cf1ecab3300a0e2fb011ee8977fa0d6fa04a5406
Cuxrie <151527066+victorsimonsen@users.noreply.github.com>
2024-10-09 19:19:22 +0200

    Add files via upload

---
+++ SportsSln/SportsStore/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+app.Run();
